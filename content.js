7dcf16f16e8ca000856110dc0f3c3bf5ccccb4e9
Moritz Tim W. <90388353+moritztim@users.noreply.github.com>
2025-01-21 01:06:38 +0100

    integrate content script with deno

--- content.js
+++
@@ -1,4 +0,0 @@
-switch (window.location.pathname) {
-	case "/zdp/einsatz":
-	// here more data should be fetched and added
-}
---
+++ src/content.ts
@@ -0,0 +1,4 @@
+switch (globalThis.location.pathname) {
+	case "/zdp/einsatz":
+		throw new Error("Not implemented");
+}
