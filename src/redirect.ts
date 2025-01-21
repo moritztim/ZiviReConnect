6bc7856d9f9e76e7c6d652acb766f79fdbacf28d
Moritz Tim W <90388353+moritztim@users.noreply.github.com>
2025-01-21 20:52:40 +0100

    redirect properly

--- src/redirect.ts
+++ src/redirect.ts
@@ -1,8 +1,12 @@
+function redirect() {
+	window.location.replace("https://ziviconnect.admin.ch/zdp")
+}
+
 // path without any / or # at the end
 switch (window.location.pathname.replace(/\/$|\/#/g, "")) {
 	case "/home":
 		// Some actions may redirect the user to the blank "home" page.
-		window.location.pathname = "/zdp";
+		redirect();
 		break;
 	case "/zdp/einsatz":
 		throw new Error("Not implemented");
