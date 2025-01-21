d71865af6764509d22d106b57bfd32494eb89ce3
Moritz Tim W <90388353+moritztim@users.noreply.github.com>
2025-01-21 21:16:54 +0100

    add redirect for ezivi

--- src/redirect.ts
+++ src/redirect.ts
@@ -2,6 +2,10 @@ function redirect() {
 	window.location.replace("https://ziviconnect.admin.ch/zdp")
 }
 
+// remove preceding www. if exists
+if (window.location.hostname.split("www.").pop() == "ezivi.admin.ch")
+	// The host of the previous zivi portal just displays a 502 error page.
+	redirect()
 // path without any / or # at the end
 switch (window.location.pathname.replace(/\/$|\/#/g, "")) {
 	case "/home":
