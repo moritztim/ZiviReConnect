f1abbabcf83b822fbebd9dad77750debdbbd3064
Moritz Tim W. <90388353+moritztim@users.noreply.github.com>
2025-01-21 02:12:24 +0100

    improve path precision

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,5 @@
-switch (window.location.pathname) {
+// path without any / or # at the end
+switch (window.location.pathname.replace(/\/$|\/#/g, "")) {
 	case "/zdp/einsatz":
 		throw new Error("Not implemented");
 }
