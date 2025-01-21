f6132cb389dde47c39b9bf1e6ec706d27b681bbf
Moritz Tim W <90388353+moritztim@users.noreply.github.com>
2025-01-21 20:43:50 +0100

    extract redirect functionality

--- src/content.ts
+++
@@ -1,9 +0,0 @@
-// path without any / or # at the end
-switch (window.location.pathname.replace(/\/$|\/#/g, "")) {
-	case "/home":
-		// Some actions may redirect the user to the blank "home" page.
-		window.location.pathname = "/zdp";
-		break;
-	case "/zdp/einsatz":
-		throw new Error("Not implemented");
-}
